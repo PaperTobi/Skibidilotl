4f94d6c94d0988401e2d153dce0f17d4f51260e0
Tobias <73227066+PaperTobi@users.noreply.github.com>
2024-11-02 01:47:43 +0100

    Update constants.go

--- constants.go
+++ constants.go
@@ -11,10 +11,10 @@ import (
 	"image/color"
 )
 
-const ReleaseUrl = "https://api.github.com/repos/Equicord/Equicord/releases/latest"
-const InstallerReleaseUrl = "https://api.github.com/repos/Equicord/Equilotl/releases/latest"
+const ReleaseUrl = "https://api.github.com/repos/PaperTobi/Skibidicord/releases/latest"
+const InstallerReleaseUrl = "https://api.github.com/repos/PaperTobi/Skibidilotl/releases/latest"
 
-var UserAgent = "Equilotl/" + buildinfo.InstallerGitHash + " (https://github.com/Equicord/Equilotl)"
+var UserAgent = "Equilotl/" + buildinfo.InstallerGitHash + " (https://github.com/PaperTobi/Skibidilotl)"
 
 var (
 	DiscordGreen  = color.RGBA{R: 0x2D, G: 0x7C, B: 0x46, A: 0xFF}
